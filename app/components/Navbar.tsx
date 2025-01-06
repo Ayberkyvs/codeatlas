e7d35a50a468c0c97df0d648b5d07c2279f4bc42
Ayberk Yavas <ayberkyavas9@gmail.com>
2025-01-06 19:40:41 +0300

    feat: :sparkles: Add NextAuth for GitHub login, create new route group, add dependencies and configurations

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,57 @@
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+import { auth, signIn, signOut } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
+const Navbar = async () => {
+	const session = await auth();
 
-export default Navbar
+	return (
+		<header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+			<nav className='flex justify-between items-center'>
+				<Link href='/'>
+					<Image
+						src='/logo.png'
+						alt='Code Atlas Brand Logo'
+						width={144}
+						height={30}
+					/>
+				</Link>
+
+				<div className='flex items-center gap-5 text-black'>
+					{session && session?.user ? (
+						<>
+							<Link href='/startup'>
+								<span>Create</span>
+							</Link>
+							<button
+								onClick={async () => {
+									"use server";
+									await signOut({redirectTo: "/"});
+								}}
+								type='button'
+							>
+								<span>Logout</span>
+							</button>
+
+							<Link href={`/user/${session?.user?.id}`}>
+								<span>{session?.user?.name}</span>
+							</Link>
+						</>
+					) : (
+						<button
+							onClick={async () => {
+								"use server";
+
+								await signIn("github");
+							}}
+							type='button'
+						>
+							<span>Login</span>
+						</button>
+					)}
+				</div>
+			</nav>
+		</header>
+	);
+};
+
+export default Navbar;
