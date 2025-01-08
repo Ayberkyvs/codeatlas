47663e6845101a5771131a4a957833a845ab3d6b
Ayberk Yavas <ayberkyavas9@gmail.com>
2025-01-08 20:11:39 +0300

    perf: :zap: Add CDN feature and Update View component

--- components/View.tsx
+++ components/View.tsx
@@ -1,6 +1,8 @@
 import Ping from '@/components/Ping';
 import { STARTUP_VIEWS_QUERY } from '../sanity/lib/queries';
 import { client } from '@/sanity/lib/client';
+import { writeClient } from '@/sanity/lib/write-client';
+import { after } from 'next/server';
 
 const View = async ({ id }: { id: string }) => {
 	const { views: totalViews } = await client
@@ -9,7 +11,12 @@ const View = async ({ id }: { id: string }) => {
 		})
 		.fetch(STARTUP_VIEWS_QUERY, { id });
 
-    // TODO: Update the number of views
+	after(async () => {
+		await writeClient
+			.patch(id)
+			.set({ views: totalViews + 1 })
+			.commit();
+	});
 	return (
 		<div>
 			<div className='view-container'>
