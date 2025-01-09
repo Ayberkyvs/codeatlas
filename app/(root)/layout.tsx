cf874afba9a541240bd29100581795937b98fd75
Ayberk Yavas <ayberkyavas9@gmail.com>
2025-01-09 17:16:09 +0300

    feat: :sparkles: Add StartupForm, form validation, and toast notifications

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '@/components/Navbar';
+import { Toaster } from '@/components/ui/toaster';
 const Layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
 	return (
 		<main>
 			<Navbar />
 			{children}
+			<Toaster />
 		</main>
 	);
 };
